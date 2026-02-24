d0d8ffc075f3d5b3a969ed665b864f07163ce50c
MILLA MANIDEEPNAGASAI <manideepnagasai123@gmail.com>
2026-02-24 19:25:11 +0530

    Update gemini_model.py

--- Autosage/gemini_model.py
+++ Autosage/gemini_model.py
@@ -1,9 +1,8 @@
 from google import generativeai as genai
 
-# 🔴 Paste your API key here
 genai.configure(api_key="AIzaSyCis98-b9qavzMoKXE4OIm4T03ui2zt9aA")
 
-# ✅ Latest supported model
+
 model = genai.GenerativeModel("gemini-2.5-flash")
 
 def get_gemini_response(prompt, image=None):
@@ -12,5 +11,6 @@ def get_gemini_response(prompt, image=None):
         response = model.generate_content([prompt, image])
     else:
         response = model.generate_content(prompt)
+
 
     return response.text
